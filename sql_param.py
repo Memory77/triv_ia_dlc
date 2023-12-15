317f7e3852e7b722a4452f8251fd04a4b0a518f4
Christian Martiny <christian.martiny@outlook.com>
2023-12-15 10:47:37 +0100

    SQL paratager avec Louis

--- sql_param.py
+++ sql_param.py
@@ -21,13 +21,29 @@ def init_db():
     # new cursor
     cur = conn.cursor()
 
-    # creation of tables
+    # CREATE TABLE "gamer"
     query_execute(cur, f'''
 CREATE TABLE IF NOT EXISTS gamer (
     id INTEGER PRIMARY KEY,
     alias TEXT NOT NULL);''')
     query_execute(cur, f'''CREATE UNIQUE INDEX IF NOT EXISTS gamer_alias_IDX ON gamer (alias);''')
-    
 
+    # CREATE TABLE "game"
+    query_execute(cur, f'''
+CREATE TABLE IF NOT EXISTS game (
+    id INTEGER PRIMARY KEY,
+    datetime TEXT NOT NULL,
+    dice_type INTEGER NOT NULL);''')
+
+    # CREATE TABLE "game_gamer"
+    query_execute(cur, f'''
+CREATE TABLE game_gamer (
+	game_id INTEGER NOT NULL,
+	gamer_id INTEGER NOT NULL,
+	alias TEXT NOT NULL,
+	score INTEGER NOT NULL,
+	CONSTRAINT game_gamer_PK PRIMARY KEY (game_id,gamer_id)
+);''')
+    
     # Fermeture de la connexion
     conn.close()
